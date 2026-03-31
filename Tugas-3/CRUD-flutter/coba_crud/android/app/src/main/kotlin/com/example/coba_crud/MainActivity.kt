f8dbd95c1bd2f48a13d1ed995aecdb74d95eabb3
windanafifiq <itsgaming243@gmail.com>
2026-03-31 11:16:18 +0700

    submit local crud

---
+++ Tugas-3/CRUD-flutter/coba_crud/android/app/src/main/kotlin/com/example/coba_crud/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.coba_crud
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ Tugas-3/CRUD-flutter/coba_crud/lib/main.dart
@@ -0,0 +1,28 @@
+import 'models/note_database.dart';
+import 'pages/notes_page.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await NoteDatabase.initialize();
+
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => NoteDatabase(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: NotesPage(),
+    );
+  }
+}
